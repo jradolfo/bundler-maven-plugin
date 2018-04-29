7b4e73ad9d5030626fa74bb0c51252f7dd8e70dc
CH3CHO <ch3cho@qq.com>
2018-04-29 14:46:23 +0800

    Fix the charset of output files.

--- src/main/java/com/github/kospiotr/bundler/ResourceAccess.java
+++ src/main/java/com/github/kospiotr/bundler/ResourceAccess.java
@@ -1,16 +1,19 @@
 package com.github.kospiotr.bundler;
 
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 class ResourceAccess {
 
+    private static final Charset CHARSET = StandardCharsets.UTF_8;
+
     public String read(Path path) {
         try {
             byte[] encoded = Files.readAllBytes(path);
-            return new String(encoded, StandardCharsets.UTF_8);
+            return new String(encoded, CHARSET);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -19,7 +22,7 @@ class ResourceAccess {
     public void write(Path path, String s) {
         try {
             Files.createDirectories(path.getParent());
-            Files.write(path, s.getBytes());
+            Files.write(path, s.getBytes(CHARSET));
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
